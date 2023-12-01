75b1bed1c5d111163b9e451b9f782dd0b78c7a0f
Burra Vinay Kumar <87492318+vinayraj02@users.noreply.github.com>
2023-12-01 07:48:04 +0530

    exec.c

---
+++ EOS/exec()/exec.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<sys/types.h>
+int main(){
+	//char *arg="/home/l1-pc18/Desktop/vinay/EOS/fork";
+	printf("...b\n");
+	printf("...b\n");
+	printf("...b\n");
+	printf("...b\n");
+	printf("...b\n");
+	
+	
+	printf("...a\n");
+	printf("...a\n");
+	printf("...a\n");
+	printf("...a\n");
+	printf("...a\n");
+	printf("...a\n");
+	execl("/home/wildsoul/Desktop/cdac/EOC/prog/fork/f","",NULL);  //give the executable file in the 1st orgument of the execl function
+	printf("...a\n");
+	}
